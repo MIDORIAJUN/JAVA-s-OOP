bf7e54caf55c4a66e8ca72528b47b8d40ecbd024
MIDORIAJUN <121005690+MIDORIAJUN@users.noreply.github.com>
2023-03-16 20:32:21 +0900

    생성자 Box클래스_258_0316

---
+++ Box.java
@@ -0,0 +1,24 @@
+package study;
+
+public class Box {
+	private int width;
+	private int length;
+	private int height;
+	private int volume;
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	Box(int w,int l,int h){
+		width = w;
+		length = l;
+		height = h;
+		volume = width * length * height;
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
